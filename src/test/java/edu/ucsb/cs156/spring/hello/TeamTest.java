69f81c91605df8e84773a8e3bb6a09f90e91ecfb
Bombboox <apetrosyan2431@gmail.com>
2026-04-08 10:34:05 -0700

    added test coverage for hashCodes

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -8,10 +8,20 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
+    Team teamEqual;
+    Team teamNotEqual;
+    Team teamEqualDifferentMembers;
+    Team teamEqualSameMembers;
+    TeamTest notTeam;
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team"); 
+
+        teamEqual = new Team("test-team"); 
+        teamNotEqual = new Team("test-team-not-equal");
+        teamEqualDifferentMembers = new Team("test-team");
+        teamEqualSameMembers = new Team("test-team");
     }
 
     @Test
@@ -19,7 +29,56 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+    @Test 
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test 
+    public void equals_returns_true_for_same_team() {
+        assert(team.equals(team));
+    }
+
+    @Test
+    public void equals_false_for_different_object_type() {
+        assert(!team.equals(notTeam));
+    }
+
+    @Test
+    public void equals_returns_true_for_equal_teams() {
+        assert(team.equals(teamEqual));
+    }
+
+    @Test
+    public void equals_returns_false_for_not_equal_teams() {
+        assert(!team.equals(teamNotEqual));
+    }
+
+    @Test 
+    public void equals_returns_false_for_different_members() {
+        teamEqualDifferentMembers.addMember("member0");
+        assert(!team.equals(teamEqualDifferentMembers));
+    }
+
+    @Test
+    public void equals_returns_true_for_same_members() {
+        teamEqualSameMembers.addMember("member1");
+        team.addMember("member1");
+        assert(team.equals(teamEqualSameMembers));
+    }
+
+    @Test
+    public void hashCode_returns_same_hashcode_for_equal_teams() {
+        assert(team.hashCode() == teamEqualSameMembers.hashCode());
+    }
+
+    @Test
+    public void correct_hashCode() {
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
